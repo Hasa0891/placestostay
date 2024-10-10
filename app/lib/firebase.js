55743aa01d71a4d948c2749e08bf8de29ff26745
Hasa0891 <mshsohel@gmail.com>
2024-10-10 21:16:58 +0600

    firebase configuration added

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app";
 import {getFireStore} from "firebase/firestore";
 
 const config = {
-    apiKey : "",
-    authDomain : "",
-    projectId : "",
-    storageBucket : "",
-    messagingSenderid : "",
-    appId : "",
-    measurementId : ""
+    apiKey: "AIzaSyCY9mhicIGR538ElGQiOqAf3MmHIGRnnU4",
+    authDomain: "placestostay-5cc87.firebaseapp.com",
+    projectId: "placestostay-5cc87",
+    storageBucket: "placestostay-5cc87.appspot.com",
+    messagingSenderId: "1033804008891",
+    appId: "1:1033804008891:web:03cda7837060ee821b889e",
+    measurementId: "G-V9Y660RYZW"
 };
 
 const app = initializeApp(config);
